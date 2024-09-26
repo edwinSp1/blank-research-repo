e9951322f57b8077b9025439c15380c5be1d2e93
Edwin Chen <115381002+edwinSp1@users.noreply.github.com>
2024-09-26 04:19:59 +0000

    Added some documentation, visualizations. Also added a trained model in learn-ssntorch/models/line_recognizer that has near 100% accuracy.

--- learn-ssntorch/model.py
+++ learn-ssntorch/model.py
@@ -19,7 +19,7 @@ from icecream import ic
 dtype = torch.float
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
 
-train_set = create_testcases()
+
 
 # Network Architecture
 num_inputs = 3*3
@@ -48,7 +48,7 @@ class Net(nn.Module):
 
         # Record the final layer
         mem2_rec = []
-
+        spk2_rec = []
         for step in range(num_steps):
             # linear transformation 1
             cur1 = self.fc1(x)
@@ -59,70 +59,76 @@ class Net(nn.Module):
             # apply leaky again
             spk2, mem2 = self.lif2(cur2, mem2)
             mem2_rec.append(mem2)
+            spk2_rec.append(spk2)
+        
+        return torch.stack(mem2_rec, dim=0), torch.stack(spk2_rec, dim=0)
 
-        return torch.stack(mem2_rec, dim=0)
-
-    
 # Load the network onto CUDA if available
 net = Net().to(device)
 
-loss = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=5e-4, betas=(0.9, 0.999))
+if __name__ == '__main__':
+    train_set = create_testcases()
 
-num_epochs = 100
+    loss = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=5e-4, betas=(0.9, 0.999))
+
+    num_epochs = 100
 
 
-for epoch in range(num_epochs):
-    for matrix_hash, label in train_set:
-        # training mode
-        net.train()
-        
-        # put the tensors onto CUDA
-        matrix_hash = matrix_hash.to(device)
-        label = label.to(device)
-
-        # get model prediction
-        mem_rec = net(matrix_hash)
-
-        # calculate loss
-        loss_val = torch.zeros((1), dtype=dtype, device=device)
-        for step in range(num_steps):
-            loss_val += loss(mem_rec[step], label)
-
-        optimizer.zero_grad()
-        loss_val.backward()
-        optimizer.step()
-    
-    with torch.no_grad():
-        correct = 0
-        total_loss = 0
+    for epoch in range(num_epochs):
         for matrix_hash, label in train_set:
+            # training mode
+            net.train()
+            
             # put the tensors onto CUDA
             matrix_hash = matrix_hash.to(device)
             label = label.to(device)
 
             # get model prediction
-            mem_rec = net(matrix_hash)
-            
+            mem_rec, spk_rec = net(matrix_hash)
+
             # calculate loss
             loss_val = torch.zeros((1), dtype=dtype, device=device)
-            amt = torch.zeros((2), dtype=dtype, device=device)
             for step in range(num_steps):
                 loss_val += loss(mem_rec[step], label)
-                amt += mem_rec[step]
-            
-            amt_array = list(amt.numpy())
-            label_array = list(label.numpy())
-            ans = 0
-            if label[1] == 1:
-                ans = 1
-            # higher probability than wrong answer
-            if amt_array[ans] > amt_array[ans^1]:
-                correct += 1
-            total_loss += loss_val
 
-        ic(epoch)
-        ic(total_loss)
-        print('accuracy:', f'{correct/len(train_set)*100}%')
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+        
+        with torch.no_grad():
+            correct = 0
+            total_loss = 0
+            for matrix_hash, label in train_set:
+                # put the tensors onto CUDA
+                matrix_hash = matrix_hash.to(device)
+                label = label.to(device)
 
-#net.save('models/line_recognizer')
+                # get model prediction
+                mem_rec, spk_rec = net(matrix_hash)
+                
+                # calculate loss
+                loss_val = torch.zeros((1), dtype=dtype, device=device)
+                amt = torch.zeros((2), dtype=dtype, device=device)
+                for step in range(num_steps):
+                    loss_val += loss(mem_rec[step], label)
+                    amt += mem_rec[step]
+                
+                amt_array = list(amt.numpy())
+                label_array = list(label.numpy())
+                ans = 0
+                if label[1] == 1:
+                    ans = 1
+                # higher probability than wrong answer
+                if amt_array[ans] > amt_array[ans^1]:
+                    correct += 1
+                total_loss += loss_val
+
+            ic(epoch)
+            ic(total_loss)
+            percentage = correct/len(train_set)*100
+            print('accuracy:', f'{percentage}%')
+            if percentage > 99.5:
+                break
+
+    torch.save(net.state_dict(), 'models/line_recognizer')
--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
-nothing to see here... for now
+Research Repository for RC and snnTorch.
+
+My plan is to expand to 10x10 after I implement the 3x3 with Reservoir Computing.
+
+To avoid overtraining, I will probably use a test set and a train set. If the model performs well on the training set but poorly on the test set I know it's overtrained. The reason my model does tests on the train set is because the train set is all 2^9 possible 3*3 matrices.
+
+The input is a tensor representing the flattened matrix, ex: tensor([1, 0, 0, 1, 1, 0, 1, 0, 1])
+
+It is then transformed similarly to visualizations/splt.animator.mp4.
+
+The output is a 2D Tensor of confidences, where tensor[i] = [confidence_no_line, confidence_line]
+
